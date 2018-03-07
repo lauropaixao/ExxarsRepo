daf96d04040383f72f7825ceb24094e14f8b7ada
Lauro Paixão <lauro2309@yahoo.com.br>
2018-03-07 15:30:30 -0300

    Inclusão do Login do Facebook

--- Login/Assets/Exxars/Scripts/FacebookInit.cs
+++ Login/Assets/Exxars/Scripts/FacebookInit.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Facebook.Unity;
+using System.Linq;
 
-public class NewBehaviourScript : MonoBehaviour {
+internal class FacebookInit : MonoBehaviour
+{
+	// Awake function from Unity's MonoBehavior
+	void Awake () 
+	{
+		if (!FB.IsInitialized) 
+		{
+			// Initialize the Facebook SDK
+			FB.Init(InitCallback, OnHideUnity);
+		} 
+		else 
+		{
+			// Already initialized, signal an app activation App Event
+			FB.ActivateApp();
+		}
+	}
+	private void InitCallback ()
+	{
+		if (FB.IsInitialized) 
+		{
+			// Signal an app activation App Event
+			FB.ActivateApp();
+			// Continue with Facebook SDK
+			// ...
+		} 
+		else
+		{
+			Debug.Log("Failed to Initialize the Facebook SDK");
+		}
+	}
+	private void OnHideUnity (bool isGameShown)
+	{
+		if (!isGameShown) {
+			// Pause the game - we will need to hide
+			Time.timeScale = 0;
+		} else {
+			// Resume the game - we're getting focus again
+			Time.timeScale = 1;
+		}
+	}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- Login/Assets/Exxars/Scripts/FacebookLogin.cs
+++ Login/Assets/Exxars/Scripts/FacebookLogin.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Facebook.Unity;
+using System.Linq;
 
-public class NewBehaviourScript : MonoBehaviour {
+internal class FacebookLogin : MonoBehaviour
+{
+	public void SignIn ()
+	{
+		var perms = new List<string>(){"public_profile", "email", "user_friends"};
+		FB.LogInWithReadPermissions(perms, AuthCallback);	
+	}
 
-	// Use this for initialization
-	void Start () {
-		
+	private void AuthCallback (ILoginResult result) 
+	{
+		if (FB.IsLoggedIn)
+		{
+			// AccessToken class will have session details
+			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+			// Print current access token's User ID
+			Debug.Log(aToken.UserId);
+			// Print current access token's granted permissions
+			foreach (string perm in aToken.Permissions)
+			{
+				Debug.Log(perm);
+			}
+		} 
+		else
+		{
+			Debug.Log("User cancelled login");
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+
 }
