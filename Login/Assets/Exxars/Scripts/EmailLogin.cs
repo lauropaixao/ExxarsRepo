e93aa659089887d72cf9ae2d8cf0dbc5224698c0
Lauro Paixão <lauro2309@yahoo.com.br>
2018-03-07 13:36:00 -0300

    Inclusão do Projeto do Login.

---
+++ Login/Assets/Exxars/Scripts/EmailLogin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine.SceneManagement;
+
+public class EmailLogin : MonoBehaviour 
+{
+	public InputField email, password;
+	public Text status;
+
+	public void SignIn ()
+	{
+		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync (email.text, password.text).ContinueWith ((obj) => 
+			{
+				status.text = "Login Realizado Com Sucesso";
+			});
+	}
+
+	public void SignUn ()
+	{
+		FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync (email.text, password.text).ContinueWith ((obj) => 
+			{
+				status.text = "Cadastro Realizado Com Sucesso";
+			});
+	}
+
+}
